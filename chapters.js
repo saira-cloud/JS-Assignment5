9d1235d56763029814e04be5bdf35efbedd8967f
hassaan qazi <sairaiqbal737@gmail.com>
2020-06-07 20:12:00 +0500

    js assignment

---
+++ chapters.js
@@ -0,0 +1,75 @@
+// // ________chapter 1__________
+
+
+// // alert("error! please enter a valid password")
+
+// // alert("welcome to JS LAND \n Happy Coding")
+
+// // alert("welcome to JS land")
+// // alert("Happy Coding \n prevent this page from additional dialogs")
+
+// // _________chapter 2________
+// // var username = "saira iqbal" ;
+// // var myName = username;
+// // alert(myName)
+
+// // var message = "hello world";
+// // alert(message)
+
+// // alert("saira iqbal")
+// // alert("25 years old")
+// // alert("mobile certified development")
+// // alert("pizza\n pizz\n piz\npi\np ")
+
+// // var email = " sairaiqbal737@gmail.com"
+// // alert("my email address is " + email)
+
+// // var book = "A smarter way to learn javascript";
+// // alert("i am trying  to learn javascript from Book" +" " + book)
+
+// // alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+
+// //  __________chapter 3_____________
+
+// // var age = 25;
+
+// // alert(" my age is " + age + "" +"years old")
+// // var birthYear = 1993;
+
+// // document.writeln("my birthday year is " + " " + birthYear)
+// // document.write("data type of my declared variable is number")
+
+// var name = prompt("enter your name", " ");
+// var proTitle = prompt("enter your enter product title", "Tshirts");
+// var quantity = prompt("enter your quantity", "5");
+// document.write(name + "ordered" + quantity + proTitle  + "on XYZ Company")
+
+// ____________chapter 4________________
+// var _name, $name, name = "saira";
+// alert(_name + $name + name)
+
+// var 1name;
+
+// var @name;
+// var &name;
+// var #name;
+// var %name;
+
+
+// var name;
+// var nmyName;
+// var $_2ndDay;
+// var my_day;
+// var _myWorld;
+
+
+
+
+
+
+
+
+
+
+
