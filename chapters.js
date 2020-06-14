2ed0586a5d1c6dfe1f819864e6c9bce84c5327eb
saira-cloud <64644888+saira-cloud@users.noreply.github.com>
2020-06-14 18:54:32 +0500

    Add files via upload

--- chapters.js
+++ chapters.js
@@ -63,6 +63,753 @@
 // var my_day;
 // var _myWorld;
 
+//___________Chap5_______________
+//_________task 1_________________
+// var num1 = prompt('enter 1st number');
+// var num2 = prompt('enter 2nd number');
+// var add =  num1 + num2;
+// document.write( "sum of " + num1 + "and" + num2 + "is" + add);
+
+// var num1 = prompt('enter 1st number');
+// var num2 = prompt('enter 2nd number');
+// var multi =  num1 * num2;
+// document.write( "sum of " + num1 + "and" + num2 + "is" + multi);
+
+// var num1 = prompt('enter 1st number');
+// var num2 = prompt('enter 2nd number');
+// var sub =  num1 - num2;
+// document.writeln( "sub of " + num1 + "and" + num2 + "is" + sub + "<br>");
+
+// var num1 = prompt('enter 1st number');
+// var num2 = prompt('enter 2nd number');
+// var mod =  num1 % num2;
+// document.writeln( "mod of  " + num1 + "and" + num2 + "is" + mod + "<br>");
+
+// var num1 = prompt('enter 1st number');
+// var num2 = prompt('enter 2nd number');
+// var div =  num1 / num2;
+// document.write( "divsion of " + num1 + "and" + num2 + "is" + div);
+
+// __________task 2________________
+
+// var name; 
+// document.writeln("the value is after variable declaration is: " + name );
+// var name = 5;
+// document.writeln("the value after initialization is :" + name);
+// name++;
+// document.writeln("the value after increment is :" + name);
+// var name1 = parseInt(name);
+// name1 +=7;
+// document.writeln("the value after initializat" + name1);
+// name1--;
+// document.writeln("the value after decrement is : " + name1);
+// var result = name1 % 3;
+// document.writeln("the remainder is : " + result);
+
+
+
+// _________task 4______________
+
+// var perCost = 600 ; 
+// var totalCost = 5 * 600;
+// document.writeln("total cost to buy 5 tickets to a movie is " + totalCost + "PKR");
+
+// ________Task 5 ______________________
+// document.write("Table of 4 <br> " );
+
+// for(var i = 1; i <=10; i ++){
+//     document.write("4" + " x" + i + "= " + 4*i + "<br>");
+
+// }
+
+
+
+
+// ______________Task 6 ____________________
+// var celTemp = 25;
+// var farhen = (celTemp * 9/5)+32;
+// document.write(celTemp + "C" + " " + "is" + farhen + "F" + "<br>" );
+
+// var celsius = (farhen - 32) * 5/9;
+// document.write(farhen + "F" + " " + "is" + celsius + "F" + "<br>" );
+
+// _______task7_____________
+
+// var priceItem1 = prompt('enter price item');
+// var priceItem2= prompt('enter price of item 2');
+// var qtyItem1 = prompt('enter the quantity of item1');
+// var qtyItem2 = prompt ('enter the quantity of item 2 ');
+// var shipping = prompt('enter shipping charges');
+
+// document.writeln("price of item 1 is " + " " + priceItem1 + "<br>" );
+// document.writeln("price of item 2 is " + " " + priceItem2 + "<br>" );
+// document.writeln("Quanty of item 1 is " + " " + qtyItem1 + "<br>" );
+// document.writeln("quantity of item 2 is " + " " + qtyItem2 + "<br>" );
+// document.writeln("Shopping Cost  " + " " + shipping + "<br>" );
+// var totalCost = ((priceItem1 * qtyItem1) + (priceItem2 * qtyItem2)) + shipping;
+
+
+// document.writeln("Total Cost of your order is " + " " + totalCost );
+
+// ___________task 8 ______________
+
+// var obtMarks = prompt('Enter obtain marks');
+// var obtMarks1 = parseInt(obtMarks);
+// var totalMarks = prompt('Enter Total Marks');
+// var totalMarks1 = parseInt(totalMarks);
+// var percentage;
+// var percentage1 = parseInt(percentage);
+// percentage1 = (obtMarks1/totalMarks1) * 100;
+
+// document.writeln("total marks " + " " + totalMarks1 + "<br>");
+// document.writeln("Obtained Marks " + " " + obtMarks1 + "<br>");
+// document.writeln("percentage" + " " + percentage1);
+
+
+// _____________task 9 ___________________
+
+
+// var dollar1 = 10; 
+// var dollar = parseInt(dollar1);
+// var saudiRiyal1 = 25;
+// var saudiRiyal = parseInt(saudiRiyal1);
+
+// var pkr1;
+// var pkr = parseInt(pkr1);
+// pkr = (dollar * 104) + (saudiRiyal1 * 28);
+
+// document.writeln("<h1> CURRENCY IN PKR</h1>")
+
+// document.writeln(" Total Currency in PKR " + " " + pkr);
+
+// _________Task 10 ______________
+
+
+// var arithmetic = 10; 
+// var arith = parseInt(arithmetic);
+
+// arith = arith +5;
+// arith = arith *10; 
+// arith = arith /2; 
+// document.writeln(' arthmetic operations are ' + arith);
+
+
+
+// _______task 11_________
+// var currYear1 = prompt('enter current year ');
+
+// var birthYear1 = prompt('enter birth year');
+// var birthYear = parseInt(birthYear1);
+// var currYear = parseInt(currYear1);
+
+// var age1; 
+// var age = parseInt(age1);
+
+// age = currYear - birthYear;
+
+// document.writeln("<h1>Age Calculator</h1> " + "<br>" );
+// document.writeln("current year " + " " + currYear + " <br>");
+// document.writeln("Birth Year " + " " + birthYear + " <br>");
+// document.writeln("Your Age " + " " + age + " <br>");
+
+
+// ____________task 12__________
+
+// var radius = prompt('enter radius:');
+
+
+// var circum1;
+// var rad= parseFloat(radius); 
+// var circum = parseFloat(circum1);
+// circum = 2 * 3.14 * rad;
+
+// document.writeln('circumference of circle is :' + circum);
+
+// var rad = prompt('enter radius: ');
+// var ar;
+// var area = parseFloat(ar);
+// var radius = parseFloat(rad);
+// area = 3.14 * (radius * radius);
+
+// document.writeln(" area of circle is : " + area);
+
+
+// ________task 13 ________________
+
+// var snack = "lays";
+
+// var currAge = 25; 
+// var currAge1 = parseInt(currAge);
+// var maxAge = 70; 
+// var maxAge1 = parseInt(maxAge);
+// var estAmount = 5; 
+// var estAmount1 = parseInt(estAmount);
+
+
+
+
+// ____________Chap 6 - 9 __________
+
+// var a1 = 10; 
+// var a = parseInt(a1);
+// document.writeln("Result: <br>");
+// document.writeln("The value of a is : " + " " + a + "<br>" );
+// document.writeln("...................................<br>");
+// ++a;
+// document.writeln("The value of a is : " + " " + a + "<br>");
+// document.writeln("Now the value of a is:" + a + " <br>" + "<br>" + "<br>");
+
+// document.writeln("The value of ++a is : " + " " + a + "<br>");
+// a++;
+// document.writeln("Now the value of a is:" + a + " <br>" + "<br>");
+
+// --a;
+// document.writeln("The value of --a is:" + a + "<br>");
+// document.writeln("Now the value of a is:" + " " + a + "<br>" + "<br>");
+
+
+// document.writeln("the value of a-- is :" + a + "<br>")
+// a--;
+// document.writeln("Now the value of a is: " + " " + a)
+
+// ________Task 2 ____________________
+
+// var a1 =2 ; 
+// var b1 = 1;
+// var a = parseInt(a1);
+// var b = parseInt(b1);
+// --a;
+// document.writeln("a will be after --a" + " " + a + "<br>");
+// var result3 = --a - --b;
+// document.writeln("output of --a and --b is:" + " " + result3 + "<br>");
+// var result1 = --a - --b + ++b;
+// document.writeln("the value of --a -  --b + --b" + " " + result1 + "<br>" + "<br>");
+// document.writeln("the value of a is :" + a + "<br>");
+// document.writeln("the value of a is :" + b + "<br>");
+// var result = --a - --b + ++b + b--;
+// document.writeln(" the result of --a - --b + ++b + b--: " + result);
+
+
+
+
+// _____________task 3 ___________________
+
+// var message = "Hello happy eid day ";
+// document.writeln(message);
+
+// ____________task 4_______
+
+// not mentioned
+
+
+// ___________task 5 ___________
+
+
+// var value2 = prompt("enter any number ");
+
+
+
+// if (var2 >=a || var2 <=z){
+    
+//     for(var i = 1; i <=10; i++){
+//         document.write(" 5 " + " x" + i + "=" + " " + 5 * i);
+
+//     }
+    
+// }
+// else if(var2 >= A || var2 <= Z){
+//     for(var i = 1; i <=10; i++){
+//         document.write(" 5 " + " x" + i + "=" + " " + 5 * i);
+
+//     }
+
+// } else {
+//     for(var i = 1; i <=10; i++){
+//         document.write(var2 + " x" + i + "=" + " " + var2 * i);
+//     }
+
+// }
+
+// ____confusion_________
+
+// ___________task 6 _________________
+
+
+// var eng = "english "; 
+//  var math = "Math";
+//  var urdu = " Urdu"; 
+
+//  var totalEng = 100; 
+//  var totalEnglish = parseInt(totalEng);
+
+//  var totalMath = 100; 
+//  var totalMath1 = parseInt(totalMath); 
+
+//  var totalUrdu = 100 ; 
+//  var totalUrdu1 = parseInt(totalUrdu); 
+
+//  var obtEng = 54; 
+//  var obtEnglish = parseInt(obtEng); 
+
+//  var obtUrdu = 54; 
+//  var obtUrdu1 = parseInt(obtUrdu); 
+
+//  var obtMath = 54; 
+//  var obtMath1 = parseInt(obtMath); 
+
+// var engResult = (obtEnglish/totalEnglish * 100);
+
+// document.writeln("<b>English</b>" + "  " + totalEnglish + "    " + obtEnglish + "\t\t" + engResult + "<br>");
+// var urduResult = (obtEnglish/totalEnglish * 100);
+
+// document.writeln("<b>Urdu</b>" + "  " + totalEnglish + "    " + obtEnglish + "\t\t" + engResult + "<br>"  );
+// document.writeln("<b>Math</b>" + "  " + totalEnglish + "    " + obtEnglish + "\t\t" + engResult + "<br>"  );
+
+
+// ______________chapter 9-11_____________
+
+// _____________task 1 _________________
+
+// var city = prompt("enter city name ");
+// if(city === "karachi"){
+//     document.writeln("Welcome to the city");
+
+// }
+
+// ____________task 2________________________
+
+// var gender = prompt('enter ur gender plz');
+
+// if( gender == "male"){
+//     document.writeln("Goodmorning Sir");
+// }
+// else 
+// {
+//     document.writeln("Goodmorning Madam");
+
+// }
+
+// _____________task 3 _____________________
+
+
+// var signal = prompt('enter signal color ');
+
+
+
+//     document.writeln("<table>")
+
+//     document.writeln("<tr>");
+//     document.writeln("<th> Signal Color </th> &nbsp &nbsp &nbsp </th>");
+//     document.writeln("<th> Message </th>");
+//     document.writeln("</tr>");
+    
+//     document.writeln("<tr>");
+//     document.writeln("<td> red </th> &nbsp &nbsp &nbsp</td>");
+//     document.writeln("<td> Must Stop</td>");
+//     document.writeln("</tr>");
+
+//     document.writeln("<tr>");
+//     document.writeln("<td> Yellow</th> &nbsp &nbsp &nbsp </td>");
+//     document.writeln("<td> Ready to Go</td>");
+//     document.writeln("</tr>");
+
+
+//     document.writeln("<tr>");
+//     document.writeln("<td> Green</th> &nbsp &nbsp &nbsp </td>");
+//     document.writeln("<td> Move Now</td>");
+//     document.writeln("</tr>");
+
+//     document.writeln("</table>");
+
+
+// ___________________task 4______________________
+
+// var fuel = prompt("enter the remaning fuel ");
+// var fuel1 = parseFloat(fuel);
+
+// if(fuel1 < 0.25){
+//    document.writeln("please refill ur fuel");
+// }
+// else
+// {
+//     document.writeln("you are save to moce");
+// }
+
+// ____________task 7_______________
+
+// document.writeln("<h1>Guess Game</h1>");
+
+// var sec = 5;
+// var secret = parseInt(sec);
+// var guess = prompt("enter your guess from 1 to 10 ");
+// var gues = parseInt(guess);
+
+
+// if(gues == secret){
+//     alert("bingo!")
+// } else if (gues == secret+1){
+//     alert("close enough to correct answer");
+// } else{
+//     alert("invalid");
+// }
+
+// ____________task 8 _____________
+
+// var num = prompt(" enter a number");
+// var num1 = parseInt(num);
+
+// if(num1%3 == 0){
+//     alert("this number is divible by 3");
+
+// }
+
+// __________task 9 _______________
+
+
+
+
+// var num1 = prompt('enter number');
+// var num = parseInt(num1);
+// if (num%2 == 0 ){
+//     alert(" its even ");
+// }
+// else if (num%3 == 0 ){
+//     alert("its odd");
+// }
+// else {
+//     alert("its wronng ")
+// }
+
+// __________task 10 _______________
+
+
+//  var temp = prompt("enter the input ");
+//   var tempp = parseInt(temp); 
+
+//   if (tempp > 10 && tempp < 20){
+//       document.writeln("OMG Today's Weather is so Cool");
+//   } 
+//   else if (tempp > 20 && tempp < 30 ){
+//     document.writeln("OMG Today's Weather is Cool");
+//   }
+//   else if (tempp > 30 && tempp < 40 ){
+//     document.writeln("the weather is normal");
+//   }
+//   else
+//   {
+//       document.writeln("its too hot outside");
+//   }
+// 
+// ___________task 11______________________
+
+// var num1 = prompt("enter first number");
+// var firstNum = parseInt(num1);
+// var num2  = prompt ("enter the second number");
+// var secNum = parseInt(num2);
+
+// var sign = prompt("enter the operator");
+
+// if( sign == "+"){
+//     var result = firstNum + secNum;
+//     document.writeln("addition operation is " + result );
+// }
+// else if( sign == "-"){
+//     var result1 = firstNum - secNum;
+//     document.writeln("subtraction operation is " + result1 );
+// }
+// else if( sign == "/"){
+//     var result2 = firstNum / secNum;
+//     document.writeln("division operation is " + result2 );
+// }
+// else if( sign == "*"){
+//     var result3 = firstNum * secNum;
+//     document.writeln("multiplication operation is " + result3 );
+// }
+
+
+
+
+
+
+// else{
+//     var result4 = firstNum % secNum;
+//     document.writeln("modulo operation is " + result4 );
+// }
+
+// ____________ chapter 12 - 13 _____________
+
+// _________task 2___________
+
+// var num1 = prompt("enter number");
+// var num = parseInt(num1);
+// var num3 = prompt("enter number 2");
+// var num2 = parseInt(num3);
+
+// if(num > num2){
+//     document.writeln(num + "is greater than" + " " + num2);
+// }
+// else if (num == num2){
+//     document.writeln(num + " is equal to " + " " + num2);
+
+// }
+
+// else {
+//     document.writeln(num + " is less than " + " " + num2);
+// }
+
+
+// _________task 3 ______________
+
+// var num1 = prompt('enter the number ')
+// var num = parseInt(num1);
+
+// if(num == 0){
+//     alert("num is 0");
+
+// }
+
+
+// else if (num > 0 ){
+//     alert("number is positive");
+// }
+// else 
+// {
+//     alert("number is negative");
+// }
+
+
+// __________________ task 4 __________
+
+// var name = prompt("enter any single character");
+
+// if(name === "a" || name === "A"){
+//     alert("its a vovel");
+// }
+// else if(name === "e" || name === "E"){
+//     alert("its a vovel");
+// }
+// else if(name === "i" || name === "I"){
+//     alert("its a vovel");
+// }
+// else if(name === "o" || name === "O"){
+//     alert("its a vovel");
+// }
+// else if(name === "u" || name === "U"){
+//     alert("its a vovel");
+// }
+// else
+// {
+//     alert("its a consonant");
+
+// }
+
+// _______________task 6_______________
+
+// var greeting ; 
+// var our = 13;
+// var hour = parseInt(our);
+// if (hour < 18){
+//     greeting = "Good Day";
+//     alert(greeting);
+// } 
+// else{
+//     alert("GoodEvening");
+// }
+
+
+// // ___________task 7 ____________
+
+// var tim = prompt('enter time');
+// var time = parseInt(tim);
+
+// if( time >= 0000 && time < 1200){
+//     alert("Good Morning");
+// }
+// else if( time >= 1200 && time < 1700){
+//     alert("Good afterNoon");
+// }
+// else if( time >= 1700 && time < 2100){
+//     alert("GOO EVENING");
+// }
+
+// else if( time >= 2100 && time <= 2359){
+//     alert("Good Night");
+// }
+// else 
+// {
+//     alert("invalid");
+// }
+
+
+// __________________Chapter 14 - 16_________
+
+// ____________task 1_______________________
+
+var students = [, , , ,];
+
+// ________________ task 2 ___________________
+
+var userObject = {}
+// ___ example of object literal with properties__
+
+var student = {
+    Firstname : "Suresy"
+}
+
+// _____________task 3 _____________
+var name = ["name1", "name2", "name3"];
+// _____________task 4_____________
+var numbers = [ 1,2,3,4,5,6,7];
+
+// ____________task 6________
+
+var mix = ["saira", 1];
+
+// _____________task 7 _____________
+
+
+
+// var arr = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.PhILL", "PhD"];
+
+// document.writeln("<h1></h1>")
+
+
+// __________________Chapter 17-20_____________
+
+// _______________________task 3 _________________
+
+// for(var i = 1; i <=10; i++){
+//     document.writeln("<div>");
+//     document.write(i + "<br>");
+
+//     document.writeln("</div>");
+
+
+// }
+
+// _______________________task 4_____________
+
+
+
+// var num = prompt('enter your table length');
+// var table = prompt('enter the table number');
+// var length=parse(num);
+// var num = parseInt(table);
+// for(var i = 1; i <=length; i++){
+
+//     document.write(num + " x" + i + "=" + " " + num*i);
+
+// }
+
+// _________________task 5 ___________
+
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for(var i = 0; i < 5 ; i++){
+    document.writeln(fruits[i] + "<br>");
+}
+
+for(var i = 0; i < 5; i++){
+
+    document.writeln(" Element at Index " + i + " " + "is" + " " + fruits[i] + "<br>");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
